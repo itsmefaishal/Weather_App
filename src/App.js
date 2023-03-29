b24b48c388c438061a68abfdb65600176c8da173
Faishal Rahman <faishal.rahman.79@gmail.com>
2023-03-30 00:53:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const API_KEY = 'cedc1849fd96dc9382fbb08416721020';
+// url = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=cedc1849fd96dc9382fbb08416721020'
+// http://api.openweathermap.org/geo/1.0/direct?q=delhi&limit={limit}&appid=cedc1849fd96dc9382fbb08416721020
+
 
 function App() {
+  const [lat,setLat] = useState(null);
+  const [lon,setLon] = useState(null);
+  const [location,setLocation] = useState('');
+  
+  const latLon = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=${API_KEY}`;
+
+  useEffect(() => {
+    axios.get(latLon).then(response => response.data)
+    .then(data => {
+      setLat(data[0].lat)
+      setLon(data[0].lon)
+    })
+  })
+  
+
+  // useEffect(() => {
+
+  //   if( lat !== null && lon !== null) {
+  //     const url = `https://api.openweathermap.org/data/2.5/weather?&units=metric&lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  //     axios.get(url).then(response => response.data)
+  //     .then(data => {
+  //     console.log(data)
+  //   })
+  //   }
+
+  // }, [lat,lon])
+
+  const searchLocation = (event) => {
+    if(event.key === 'Enter' ) {
+      if(lat !== null && lon !== null){
+        const url = `https://api.openweathermap.org/data/2.5/weather?&units=metric&lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    const latLon = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=${API_KEY}`;
+
+      axios.get(url).then(response => response.data)
+      .then(data => {
+      console.log(data)
+      })
+    }
+    setLocation('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    <input
+      value={location}
+      onChange={event => setLocation(event.target.value)}
+      onKeyPress={searchLocation}
+      placeholder='Enter location'
+      type="text" />     
+      <p>latitude : {lat}</p>
+      <p>longitude : {lon}</p>
     </div>
   );
+}
 }
 
 export default App;
