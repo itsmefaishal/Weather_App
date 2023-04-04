3c8cbb3a7d1f687dd8eacfed43844023643684fc
Faishal Rahman <faishal.rahman.79@gmail.com>
2023-04-04 13:56:29 +0530

    updating bulk useState() with useReducer()

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer, useState, useEffect } from 'react';
 import axios from 'axios';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
+const initialState = {
+  lat : "",
+  lon : "",
+  temp : "",
+  humidity : "",
+  speed : "",
+  weather : "",
+  location : "",
+  tempMax : "",
+  tempMin : "",
+  seaLevel : "",
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_LAT" :
+      return { ...state, lat : action.payload };
+    case "SET_LON" : 
+      return { ...state, lon : action.payload };
+    case "SET_TEMP" : 
+      return { ...state, temp : action.payload };
+    case "SET_HUMIDITY" : 
+      return { ...state, humidity : action.payload };
+    case "SET_SPEED" : 
+      return { ...state, speed : action.payload };
+    case "SET_WEATHER" : 
+      return { ...state, weather : action.payload };
+    case "SET_LOCATION" : 
+      return { ...state, location : action.payload };
+    case "SET_TEMPMAX" : 
+      return { ...state, tempMax : action.payload };
+    case "SET_TEMPMIN" : 
+      return { ...state, tempMin : action.payload };
+    case "SET_SEALEVEL" : 
+      return { ...state, seaLevel : action.payload };
+    default:
+      return state;
+  }
+}
+
 function App() {
-  const [lat,setLat] = useState(null);
-  const [lon,setLon] = useState(null);
-  const [temp,setTemp] = useState("");
-  const [humidity, setHumidity] = useState('');
-  const [speed,setSpeed] = useState('');
-  const [weatherDescription, setWeatherDescription] = useState('');
+  
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { lat, lon, temp, humidity, speed, weather, tempMin, tempMax, seaLevel } = state;
   const [location,setLocation] = useState('');
-  const [tempMin, setTempMin] = useState('');
-  const [tempMax, setTempMax] = useState('');
-  const [seaLevel, setSealevel] = useState('');
   
   const latLon = `https://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=${API_KEY}`;
 
@@ -21,8 +56,8 @@ function App() {
     if(location){
       axios.get(latLon).then(response => response.data)
       .then(data => {
-      setLat(data[0].lat)
-      setLon(data[0].lon)
+       dispatch({ type : "SET_LAT" , payload : data[0].lat})
+       dispatch({ type : "SET_LON" , payload : data[0].lon})
     })
     }
   },[location, latLon])
@@ -34,13 +69,13 @@ function App() {
       const url = `https://api.openweathermap.org/data/2.5/weather?&units=metric&lat=${lat}&lon=${lon}&appid=${API_KEY}`;
       axios.get(url).then(response => response.data)
       .then(data => {
-      setTemp(data.main.temp.toFixed(0))
-      setHumidity(data.main.humidity)
-      setTempMax(data.main.temp_max)
-      setTempMin(data.main.temp_min)
-      setSealevel(data.main.sea_level)
-      setSpeed(data.wind.speed)
-      setWeatherDescription(data.weather[0].description)
+        dispatch({ type : "SET_TEMP", payload : data.main.temp.toFixed(0)})
+        dispatch({ type : "SET_HUMIDITY" , payload : data.main.humidity})
+        dispatch({ type : "SET_SPEED" ,payload : data.wind.speed})
+        dispatch({ type : "SET_TEMPMAX", payload : data.main.temp_max})
+        dispatch({ type : "SET_TEMPMIN", payload : data.main.temp_min})
+        dispatch({ type : "SET_SEALEVEL", payload : data.main.sea_level})
+        dispatch({ type : "SET_WEATHER", payload : data.weather[0].description})
     })
     }
 
@@ -71,7 +106,7 @@ function App() {
         <div className="top">    
           <p>{location}</p>
           <h1>{temp}°C</h1>
-          <p>{weatherDescription}</p>   
+          <p>{weather}</p>   
         </div>
         <div className="middle">
           <p><span>Maximum temperature</span><span><b>{tempMax}°C</b></span></p>
